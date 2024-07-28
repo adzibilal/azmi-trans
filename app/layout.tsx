cae35eeb154eb838a1f6d1e1062ab2ab95ecc5d2
adzibilal <adzibilal02@gmail.com>
2024-07-29 00:00:19 +0700

    feat: update phone number in HeaderAccents component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
+import Script from 'next/script'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -16,6 +19,25 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
+            <Head>
+                <title>AZMI TRANS</title>
+                <meta name='description' content='AMAN NYAMAN EKONOMIS' />
+                <link rel='icon' href='/favicon.ico' />
+                {/* Google tag (gtag.js) */}
+                <script
+                    async
+                    src='https://www.googletagmanager.com/gtag/js?id=AW-965062289'></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'AW-965062289');
+                        `
+                    }}
+                />
+            </Head>
             <body className={montserrat.className}>{children}</body>
         </html>
     )
